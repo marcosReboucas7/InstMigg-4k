ea3e970c77c4e6dd7ddeb776406802fe7bfb3138
Marcos Rebouças <marcosbest12@gmail.com>
2025-10-30 17:58:16 -0300

    Criada a primeira migration e ajustado o banco de dados. Problema de erro de salvamento de novos cadastros ajustado.

---
+++ Migrations/20251030204225_Iniciar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstMiggD.Migrations
+{
+    /// <inheritdoc />
+    public partial class Iniciar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Contract = table.Column<string>(type: "TEXT", nullable: true),
+                    NewPrice = table.Column<double>(type: "REAL", nullable: false),
+                    NewContract = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
